edafcb049581abc7ff287aaaf984d52293bc9248
lucas melo <lucas.melo044@gmail.com>
2022-10-26 09:43:50 -0300

    add main, geraLink, pegaurl e renomeado a funcao lerJson

--- tw-updater.py
+++ tw-updater.py
@@ -16,11 +16,11 @@ def cria_streamer():
 
 
     arquivo = open('streamer.m3u8', 'w+')
-    arquivo.write('base')
+    arquivo.write(base)
     arquivo.close()
 
 
-def lerjson():
+def lerJson():
     array = []
     arquivo = open('streamers.json', 'r')
     nomes = json.load(arquivo)
@@ -30,6 +30,35 @@ def lerjson():
 
     return array 
 
+def pegaUrl(nome):
+    url = f'https://twitch.tv/{nome}'
+    return url
+
+def escreveLink(link, nome):
+    with open('streamer.m3u8', 'a') as arquivo:
+        text = f'#EXTINF:-1, {str(nome).capitalize()}\n{link}\n\n'
+
+        arquivo.write(text)
+        arquivo.close()
+
+def geraLink(url, nome):
+    ytdl = youtube_dl.YoutubeDL()
+
+    try:
+        stream_link = ytdl.extract_info(url, download=False)
+        return stream_link['url']
+    except youtube_dl.utils.DownloadError:
+        print(f'O Streamer {nome} esta Offline')
+
+def main():
+    cria_streamer()
+    nomes = lerJson()
+    
+    for nome in nomes:
+        url = pegaUrl(nome)
+        link = geraLink(url, nome)
+        escreveLink(link, nome)
+
 
 if __name__ == '__main__':
-    pass
+    main()
