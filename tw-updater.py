eb64c62d6927761a2ca52743349d9b0165bf7e3a
lucas melo <lucas.melo044@gmail.com>
2022-10-26 08:33:10 -0300

    add funcao criar streamer

--- tw-updater.py
+++ tw-updater.py
@@ -5,7 +5,7 @@ import youtube_dl
 import json
 import os
 
-def lerjson():
+def cria_streamer():
     base = '#EXTM3U8\n\n'
 
     os.system('rm streamer.m3u8')
@@ -13,6 +13,9 @@ def lerjson():
     arquivo = open('streamer.m3u8', 'w+')
     arquivo.write('base')
     arquivo.close()
+
+
+def lerjson():
 
 if __name__ == '__main__':
     lerjson()
